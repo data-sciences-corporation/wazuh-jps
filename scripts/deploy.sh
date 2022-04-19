3bc96181ea62a05c2952822e97bd03e5fb820bec
Richard Raymond <44724035+Umlatt@users.noreply.github.com>
2022-04-19 18:07:35 +0200

    Changed logfile name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 fqdn=$1
 public_ip=$2
 script="deploy.sh"
-logfile="/var/log/jps-3cx-install.log"
+logfile="/var/log/jps-wazuh-install.log"
 echo "[$script] Downloading latest build."  >> $logfile
 git clone https://github.com/wazuh/wazuh-docker.git -b v4.2.6 --depth=1
 cd wazuh-docker
